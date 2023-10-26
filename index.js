c01010e741f7c7685a6485468fb808bfb22bbdff
AirplaneGoBrr <mottlcompound@gmail.com>
2023-10-26 10:35:28 -0700

    First utils!

---
+++ index.js
@@ -0,0 +1,17 @@
+let rand = function() {
+    return Math.random().toString(36).substr(2); // remove `0.`
+};
+
+let token = function(length = 2) {
+    let out = ""
+    for (let index = 0; index < length; index++) {
+        out += rand()
+    }
+    return out
+};
+
+module.exports = {
+    token,
+    rand,
+    models: require("./models")
+}
---
+++ models.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+
+const statusPageHosts = mongoose.model('statusPageHosts', new mongoose.Schema({
+    statusID: { type: String, },
+    hostUID: { type: String },
+    host: { type: String },
+    displayName: { type: String },
+    group: { type: String }
+}, {collation: { locale: 'en_US', strength: 1 }, collection: "hosts"}));
+
+const statusPageHostExtra = mongoose.model('statusPageHostExtra', new mongoose.Schema({
+    statusID: { type: String, },
+    hostUID: { type: String },
+    host: { type: String },
+    displayName: { type: String },
+    group: { type: String }
+}, {collation: { locale: 'en_US', strength: 1 }, collection: "hostsExtra"}));
+
+const statusPageHostInfo = mongoose.model('statusPageHostInfo', new mongoose.Schema({
+    statusID: { type: String, },
+    hostUID: { type: String },
+    date: { type: String },
+    data: { type: Array }
+}, {collation: { locale: 'en_US', strength: 1 }, collection: "data"}));
+
+const statusUser = mongoose.model('statusUser', new mongoose.Schema({
+    username: { type: String, },
+    password: { type: String },
+    token: { type: String },
+    id: { type: String }
+}, {collation: { locale: 'en_US', strength: 1 }, collection: "users"}));
+
+const statusIncidents = mongoose.model('statusIncidents', new mongoose.Schema({
+    statusID: { type: String, },
+    hostUID: { type: String },
+    incidentID: { type: String },
+    date: { type: String },
+    title: { type: String, },
+    description: { type: String },
+    ETA: { type: String },
+    reportedBy: { type: String }
+}, {collation: { locale: 'en_US', strength: 1 }, collection: "incidents"}));
+
+async function login(uri) {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }).then(() => resolve);
+    })
+}
+
+module.exports = {
+    mongoose,
+    statusPageHosts,
+    statusPageHostInfo,
+    statusUser,
+    statusIncidents,
+    statusPageHostExtra,
+    login
+}
