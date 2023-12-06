545b8a646e34fe008dd2374d59a6c773b70e8460
AirplaneGoBrr <mottlcompound@gmail.com>
2023-12-06 13:53:08 -0800

    2.0.0 update to match bot code

--- models.js
+++ models.js
@@ -3,38 +3,48 @@ const mongoose = require('mongoose');
 const statusPageHosts = mongoose.model('statusPageHosts', new mongoose.Schema({
     statusID: { type: String, },
     statusName: { type: String },
-    hostUID: { type: String },
+    groupID: { type: String },
     host: { type: String },
     displayName: { type: String },
-    group: { type: String },
-    userID: { type: String }
+    userID: { type: String },
+    // showIP: { type: Boolean },
 }, {collation: { locale: 'en_US', strength: 1 }, collection: "hosts"}));
 
-
-const statusPageHostInfo = mongoose.model('statusPageHostInfo', new mongoose.Schema({
+const statusPageHostData = mongoose.model('statusPageHostData', new mongoose.Schema({
     statusID: { type: String, },
-    hostUID: { type: String },
+    groupID: { type: String },
     date: { type: String },
     data: { type: Array }
 }, {collation: { locale: 'en_US', strength: 1 }, collection: "data"}));
 
+// TODO: We might remove this
 const statusUser = mongoose.model('statusUser', new mongoose.Schema({
     username: { type: String, },
     password: { type: String },
     token: { type: String },
     id: { type: String },
-    statusIDs: { type: Array }
+    statusIDs: { type: Array },
+    groupIDs: { type: Array }
 }, {collation: { locale: 'en_US', strength: 1 }, collection: "users"}));
 
 const statusIncidents = mongoose.model('statusIncidents', new mongoose.Schema({
     statusID: { type: String, },
-    hostUID: { type: String },
+    groupID: { type: String },
     incidentID: { type: String },
     date: { type: String },
     title: { type: String, },
     description: { type: String },
     ETA: { type: String },
     reportedBy: { type: String }
+}, {collation: { locale: 'en_US', strength: 1 }, collection: "incidents"}));
+
+const statusGroupInfo = mongoose.model('statusGroupInfo', new mongoose.Schema({
+    statusIDs: { type: Array, },
+    groupID: { type: String },
+    groupName: { type: String },
+    channelID: { type: String },
+    guildID: { type: String },
+    messageID: { type: String }
 }, {collation: { locale: 'en_US', strength: 1 }, collection: "incidents"}));
 
 async function login(uri) {
@@ -53,8 +63,9 @@ async function login(uri) {
 module.exports = {
     mongoose,
     statusPageHosts,
-    statusPageHostInfo,
+    statusPageHostData,
     statusUser,
     statusIncidents,
+    statusGroupInfo,
     login
 }
